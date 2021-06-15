f31b7f14ab5f44158306e236ab48791006fb24c7
Max Lambrecht <maxlambrecht@gmail.com>
2021-06-15 10:30:17 -0300

    Remove 'build.rs'.
    Signed-off-by: Max Lambrecht <maxlambrecht@gmail.com>

--- build.rs
+++
@@ -1,5 +0,0 @@
-fn main() {
-    let proto_root = "src/proto";
-    protoc_grpcio::compile_grpc_protos(&["workload.proto"], &[proto_root], &proto_root, None)
-        .expect("Failed to compile gRPC definitions!");
-}
