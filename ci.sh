b58ee4911d9fe04bf4629d39f7279c0912dfb3c8
Max Lambrecht <maxlambrecht@gmail.com>
2023-08-06 14:02:19 -0500

    Minor amendment
    Signed-off-by: Max Lambrecht <maxlambrecht@gmail.com>

--- ci.sh
+++ ci.sh
@@ -68,7 +68,7 @@ fi
 
 # Register the workload through UID with the SPIFFE ID "spiffe://example.org/myservice" with a TTL of 5 seconds
 bin/spire-server entry create -parentID ${agent_id} -spiffeID spiffe://example.org/myservice -selector unix:uid:$(id -u) -ttl 5
-sleep 3
+sleep 10  # this value is derived from the default Agent sync interval
 # Register the workload through UID with the SPIFFE ID "spiffe://example.org/myservice2" with a TTL of 5 seconds
 bin/spire-server entry create -parentID ${agent_id} -spiffeID spiffe://example.org/myservice2 -selector unix:uid:$(id -u) -ttl 5
 sleep 10  # this value is derived from the default Agent sync interval
