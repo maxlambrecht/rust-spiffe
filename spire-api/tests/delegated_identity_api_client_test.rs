50906f1329ae4b4208f7efd66954c52b2426f9e1
Max Lambrecht <maxlambrecht@gmail.com>
2023-08-13 16:00:37 -0500

    Refactor repository and add spire-api crate and spire-api-sdk submodule
    Signed-off-by: Max Lambrecht <maxlambrecht@gmail.com>

---
+++ spire-api/tests/delegated_identity_api_client_test.rs
@@ -0,0 +1,10 @@
+// These tests requires a running SPIRE server and agent (see `scripts/run-spire.sh`).
+
+#[cfg(feature = "integration-tests")]
+mod integration_tests {
+
+    #[test]
+    fn dummy_test() {
+        assert!(true);
+    }
+}
