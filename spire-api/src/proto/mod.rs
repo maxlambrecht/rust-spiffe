8b74f1aeeeb9a179c9b74d23165d1e0ddc1470c1
Max Lambrecht <maxlambrecht@gmail.com>
2023-08-14 10:26:49 -0500

    Refactor repository: new `spiffe` folder, new `spire-api` crate, and `spire-api-sdk` submodule (#42)
    Signed-off-by: Max Lambrecht <maxlambrecht@gmail.com>

---
+++ spire-api/src/proto/mod.rs
@@ -0,0 +1,15 @@
+pub mod spire {
+    pub mod api {
+        pub mod agent {
+            pub mod delegatedidentity {
+                pub mod v1 {
+                    include!("spire.api.agent.delegatedidentity.v1.rs");
+                }
+            }
+        }
+
+        pub mod types {
+            include!("spire.api.types.rs");
+        }
+    }
+}
