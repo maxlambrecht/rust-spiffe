59e79ebdbbd4573d6c0eeab9433157466b6356f3
Max Lambrecht <maxlambrecht@gmail.com>
2023-08-23 15:14:11 -0500

    Fix build.rs in spire-api
    Signed-off-by: Max Lambrecht <maxlambrecht@gmail.com>

--- spire-api/build.rs
+++ spire-api/build.rs
@@ -1,4 +1,22 @@
-fn main() -> Result<(), anyhow::Error> {
+use std::path::Path;
+use std::process::Command;
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let destination = Path::new("../spire-api-sdk");
+    if !destination.exists() {
+        let status = Command::new("git")
+            .args(&[
+                "clone",
+                "https://github.com/spiffe/spire-api-sdk.git",
+                destination.to_str().unwrap(),
+            ])
+            .status()?;
+
+        if !status.success() {
+            return Err("Failed to clone repository".into());
+        }
+    }
+
     let mut proto_config = prost_build::Config::new();
     proto_config.bytes(["."]);
     tonic_build::configure()
