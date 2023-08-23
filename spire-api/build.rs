e896b04a65ef50b24dc3e72276fb0f616cc1ad51
Max Lambrecht <maxlambrecht@gmail.com>
2023-08-23 16:42:58 -0500

    Pin build process to spire-api-sdk tag
    Signed-off-by: Max Lambrecht <maxlambrecht@gmail.com>

--- spire-api/build.rs
+++ spire-api/build.rs
@@ -1,6 +1,8 @@
 use std::path::Path;
 use std::process::Command;
 
+const SPIRE_API_SDK_TAG: &str = "v1.7.2";
+
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let destination = Path::new("../spire-api-sdk");
     if !destination.exists() {
@@ -15,6 +17,16 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         if !status.success() {
             return Err("Failed to clone repository".into());
         }
+
+        // Checkout the specific tag
+        let status = Command::new("git")
+            .current_dir(destination)
+            .args(&["checkout", SPIRE_API_SDK_TAG])
+            .status()?;
+
+        if !status.success() {
+            return Err("Failed to checkout tag".into());
+        }
     }
 
     let mut proto_config = prost_build::Config::new();
@@ -25,7 +37,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         .compile_with_config(
             proto_config,
             &["../spire-api-sdk/proto/spire/api/agent/delegatedidentity/v1/delegatedidentity.proto"],
-            &["../spire-api-sdk/proto"], 
+            &["../spire-api-sdk/proto"],
         )?;
 
     Ok(())
