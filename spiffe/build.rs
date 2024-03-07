68f5dc66f2398feb87d0543163fdec99b0b5f005
Max Lambrecht <maxlambrecht@gmail.com>
2024-03-07 09:27:28 -0600

    Skipping protobuf code generation on docs.rs (#74)
    Signed-off-by: Max Lambrecht <maxlambrecht@gmail.com>

--- spiffe/build.rs
+++ spiffe/build.rs
@@ -1,14 +1,21 @@
-use std::fs;
+use std::{env, fs};
 
 fn main() -> Result<(), anyhow::Error> {
-    let mut proto_config = prost_build::Config::new();
-    proto_config.bytes(["."]);
-    tonic_build::configure()
-        .build_client(true)
-        .out_dir("src/proto")
-        .compile_with_config(proto_config, &["src/proto/workload.proto"], &["src/proto"])?;
+    // Check if this is a docs.rs build
+    let is_docs_rs = env::var_os("DOCS_RS").is_some();
 
-    fs::rename("src/proto/_.rs", "src/proto/workload.rs")?;
+    if !is_docs_rs {
+        let mut proto_config = prost_build::Config::new();
+        proto_config.bytes(["."]);
+        tonic_build::configure()
+            .build_client(true)
+            .out_dir("src/proto")
+            .compile_with_config(proto_config, &["src/proto/workload.proto"], &["src/proto"])?;
+
+        fs::rename("src/proto/_.rs", "src/proto/workload.rs")?;
+    } else {
+        println!("cargo:warning=Skipping protobuf code generation on docs.rs.");
+    }
 
     Ok(())
 }
--- spire-api/build.rs
+++ spire-api/build.rs
@@ -1,14 +1,23 @@
+use std::env;
+
 fn main() -> Result<(), anyhow::Error> {
-    let mut proto_config = prost_build::Config::new();
-    proto_config.bytes(["."]);
-    tonic_build::configure()
-        .build_client(true)
-        .out_dir("src/proto")
-        .compile_with_config(
-            proto_config,
-            &["spire-api-sdk/proto/spire/api/agent/delegatedidentity/v1/delegatedidentity.proto"],
-            &["spire-api-sdk/proto"], 
-        )?;
+    // Check if this is a docs.rs build
+    let is_docs_rs = env::var_os("DOCS_RS").is_some();
+
+    if !is_docs_rs {
+        let mut proto_config = prost_build::Config::new();
+        proto_config.bytes(["."]);
+        tonic_build::configure()
+            .build_client(true)
+            .out_dir("src/proto")
+            .compile_with_config(
+                proto_config,
+                &["spire-api-sdk/proto/spire/api/agent/delegatedidentity/v1/delegatedidentity.proto"],
+                &["spire-api-sdk/proto"],
+            )?;
+    } else {
+        println!("cargo:warning=Skipping protobuf code generation on docs.rs.");
+    }
 
     Ok(())
 }
