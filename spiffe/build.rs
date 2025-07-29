cb472e85bc4e449f13de397f736898c006ce0644
Max Lambrecht <maxlambrecht@gmail.com>
2025-07-29 18:08:36 -0500

    Bump tonic and prost dependencies (#165)
    Signed-off-by: Max Lambrecht <maxlambrecht@gmail.com>
    Co-authored-by: Ben Leggett <benjamin@edera.io>

--- spiffe/build.rs
+++ spiffe/build.rs
@@ -21,10 +21,10 @@ fn main() -> anyhow::Result<()> {
     let file_descriptors = protox::compile(["src/proto/workload.proto"], ["src/proto"])
         .context("failed to compile protocol buffer file set")?;
 
-    tonic_build::configure()
+    tonic_prost_build::configure()
         .build_client(true)
         .out_dir(out_dir)
-        .compile_fds_with_config(proto_config, file_descriptors)
+        .compile_fds_with_config(file_descriptors, proto_config)
         .context("failed to compile protocol buffers")?;
 
     let workload = out_dir.join("_.rs");
